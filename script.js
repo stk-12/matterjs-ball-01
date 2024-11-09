b0144cb18a7342c4d59646d3826f147c29889dd2
keisuke sato <corydoras121@gmail.com>
2024-11-09 14:38:39 +0900

    add texture

--- script.js
+++ script.js
@@ -19,32 +19,41 @@ const render = Render.create({
     options: {
       width: window.innerWidth,
       height: window.innerHeight,
+      wireframes: false, // ワイヤーフレーム
     }
 });
 
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
+// var boxA = Bodies.rectangle(400, 200, 80, 80);
+// var boxB = Bodies.rectangle(450, 50, 80, 80);
 
 const ball = Bodies.circle(
-  100, // 円の中心位置（x座標）
-  100, // 円の中心位置（y座標）
+  window.innerWidth / 2, // 円の中心位置（x座標）
+  window.innerHeight / 4, // 円の中心位置（y座標）
   50, // 半径
   {
     // isStatic: false, // trueにすると静的なオブジェクトになる
-    restitution: 0.9 // 弾性係数
+    restitution: 0.9, // 弾性係数
+    render: {
+      sprite: {
+        texture: './ball.png',
+        xScale: 0.125,
+        yScale: 0.125
+      }
+    },
   }
 );
 
+// ワールドにすべてのボディ（オブジェクト）を追加
 Composite.add(world, [
-  Bodies.rectangle(window.innerWidth / 2, 0, window.innerWidth, 50, { isStatic: true }),    // 上の壁
+  Bodies.rectangle(window.innerWidth / 2, 0, window.innerWidth, 50, { isStatic: true }), // 上の壁
   Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 50, { isStatic: true }), // 下の壁
   Bodies.rectangle(window.innerWidth, window.innerHeight / 2, 50, window.innerHeight, { isStatic: true }), // 右の壁
-  Bodies.rectangle(0, window.innerHeight / 2, 50, window.innerHeight, { isStatic: true })   // 左の壁
+  Bodies.rectangle(0, window.innerHeight / 2, 50, window.innerHeight, { isStatic: true }), // 左の壁
+  ball,
 ]);
 
-// ワールドにすべてのボディ（オブジェクト）を追加
-Composite.add(world, [boxA, boxB, ball]);
+// Composite.add(world, [ball]);
 
 // マウス制御
 const mouse = Mouse.create(render.canvas);
